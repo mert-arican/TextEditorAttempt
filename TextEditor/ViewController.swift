3817a894573cd672b2f8fa8e23cefaa881018362
Mert Arıcan <mert-arican@outlook.com>
2023-08-20 18:00:35 +0300

    Initial Commit

---
+++ TextEditor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TextEditor
+//
+//  Created by Mert Arıcan on 20.08.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
